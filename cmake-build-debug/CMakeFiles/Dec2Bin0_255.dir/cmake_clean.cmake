b38586770ae38674cc65389d7958ad3cd0074e6b
DrThuner <72319460+DrThuner@users.noreply.github.com>
2021-03-23 18:48:34 +0100

    Dec2bin

---
+++ cmake-build-debug/CMakeFiles/Dec2Bin0_255.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dec2Bin0_255.dir/main.c.obj"
+  "Dec2Bin0_255.exe"
+  "Dec2Bin0_255.exe.manifest"
+  "Dec2Bin0_255.pdb"
+  "libDec2Bin0_255.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dec2Bin0_255.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+long int dec_to_bin(int dec) {              // Funkcja konwertuje liczbe dziesiatna na binarna
+    long int binary = 0;
+    int remainder, i = 1, flag = 1;
+    while (dec != 0) {
+        remainder = dec % 2;
+        dec /= 2;
+        binary += remainder * i;
+        i = i * 10;
+    }
+    return binary;
+}
+
+int main() {
+    int dec;
+    printf("Wprowadz liczbe dziesietna: ");
+    scanf("%d", &dec);
+    printf("Liczba binarna to: %d\n", dec_to_bin(dec));
+    return 0;
+}
